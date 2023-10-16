6fa87cbb50ec208d1873a4debe5fe9144a5cd191
abdullah-25 <imv.abdullah@mail.utoronto.ca>
2023-10-16 11:09:14 -0400

    clean token check when none is provided

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-const { ApolloServer, gql, GraphQLError } = require("apollo-server");
+const { ApolloServer } = require("apollo-server");
 const { resolvers, typeDefs } = require("./schemas/schema");
 import { getUser } from "./authUtils";
 
@@ -6,12 +6,11 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: async ({ req }: any) => {
-    //split method added to extract only the key and not the word 'bearer'
-    const token = req.headers.authorization.split(" ")[1] || "";
+    const token = req.headers.authorization || "";
 
     //when checking for queries that dont need auth such as query all movies..
     //..we return empty so context can work even without token
-    if (token === "" || token === undefined || token === null) return {};
+    if (token === "") return {};
 
     if (token) {
       //invoke getUser method from wuthUtils to verify if user is authenticated
